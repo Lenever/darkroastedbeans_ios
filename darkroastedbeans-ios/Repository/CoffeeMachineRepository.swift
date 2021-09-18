a01bc1c59a677fac459e90ecb0f10dfd9412517b
Ikechukwu Onuorah <40242544+Lenever@users.noreply.github.com>
2021-09-18 20:57:43 +0100

    add coffee machine repository

---
+++ darkroastedbeans-ios/Repository/CoffeeMachineRepository.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeMachineRepository.swift
+//  darkroastedbeans-ios
+//
+//  Created by Ikechukwu Onuorah on 18/09/2021.
+//
+
+import Foundation
+import Combine
+
+enum CoffeeMachineRepository {
+    static let apiClient = APIClient()
+    static let baseUrl = URL(string: "https://darkroastedbeans.coffeeit.nl/")!
+}
+
+enum APIPath: String {
+    case coffeeMachine = "coffee-machine/60ba1ab72e35f2d9c786c610"
+}
+
+extension CoffeeMachineRepository {
+    static func request(_ path: APIPath) -> AnyPublisher<CoffeeMachine, Error> {
+        guard var components = URLComponents(url: baseUrl.appendingPathComponent(path.rawValue), resolvingAgainstBaseURL: true)
+            else { fatalError("Couldn't create URLComponents") }
+//        components.queryItems = [URLQueryItem(name: "api_key", value: "your_api_key_here")]
+        
+        let request = URLRequest(url: components.url!)
+        
+        return apiClient.run(request)
+            .map(\.value)
+            .eraseToAnyPublisher()
+    }
+}
